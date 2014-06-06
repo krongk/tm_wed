11520828d91b595be8cd06580694bfb44468e910
krongk <kenrome@gmail.com>
2014-06-07 00:43:15 +0800

    Add mb

--- app/models/site_image.rb
+++ app/models/site_image.rb
@@ -6,7 +6,7 @@ class SiteImage < ActiveRecord::Base
                     :styles => {:original => '640x960#'} #override the original file
                     #:styles => { :content => '640x960>' } #standerd mobile size: 320*480 480*800 640*960
 
-  validates_attachment_size :image, :less_than => 4.megabytes
+  validates_attachment_size :image, :less_than => 8.megabytes
   validates_attachment_presence :image
 
   #update to version 4.0
