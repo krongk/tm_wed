6881a96d819682ac193387ae699311462549ddfd
krongk <kenrome@gmail.com>
2014-06-20 00:48:12 +0800

    update

--- app/helpers/sites_helper.rb
+++ app/helpers/sites_helper.rb
@@ -4,6 +4,11 @@ module SitesHelper
     site_payment = site.site_payment
     case site_payment.state
     when 'opening'; '等待付款'
+    when 'pending'; '等待发货'
+    when 'paid'; '等待收货'
+    when 'completed'; '付款成功'
+    when 'canceled'; '已经取消'
+    when 'expired'; '已经过期'
     else '未知'
     end
   end
