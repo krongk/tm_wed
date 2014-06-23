8cfe4ad82c8007352d0b75e010cc6280edb8242f
krongk <kenrome@gmail.com>
2014-06-23 12:50:16 +0800

    Affe

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
-  include ApplicationHelper
+  helper :all
+  before_filter :prepare_for_mobile
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
@@ -30,4 +32,12 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  private
+    def mobile_device?
+      request.user_agent =~ /Mobile|webOS/
+    end
+    helper_method :mobile_device?
+    def prepare_for_mobile
+      session[:mobile_param] = params[:mobile] if params[:mobile]
+    end
 end
