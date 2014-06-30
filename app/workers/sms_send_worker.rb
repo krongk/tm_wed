44943eb9d1ccd2cfc00db8dc5b49ed9dc9ad76ca
krongk <kenrome@gmail.com>
2014-06-30 10:22:17 +0800

    update session login

--- app/workers/sms_send_worker.rb
+++ app/workers/sms_send_worker.rb
@@ -2,7 +2,7 @@ class SmsSendWorker
   include Sidekiq::Worker
 
   def perform(mobile_phone, content)
-    logger.debug "sms send to: #{mobile_phone}"
+    puts "sms send to: #{mobile_phone}"
     status_id = SmsBao.send(ENV['SMS_BAO_USER'], ENV['SMS_BAO_PASSWORD'], mobile_phone, content)
     #SitePage::Keystore.increment_value_for("sms_send:#{mobile_phone}", status_id)
   end
--- app/workers/token_send_worker.rb
+++ app/workers/token_send_worker.rb
@@ -2,6 +2,7 @@ class TokenSendWorker
   include Sidekiq::Worker
 
   def perform(mobile_phone, token)
+    puts "token send to #{mobile_phone}"
     content = "登录验证码#{token}，请在10分钟内登录网站验证【维斗喜帖】"
     status_id = SmsBao.send(ENV['SMS_BAO_USER'], ENV['SMS_BAO_PASSWORD'], mobile_phone, content)
     #SitePage::Keystore.increment_value_for("sms_send:#{mobile_phone}", status_id)
