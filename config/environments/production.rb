3d11db3ccd610f91d097bb3cb6ebd263c0e74dab
krongk <kenrome@gmail.com>
2014-07-24 09:52:36 +0800

    update application.rba

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,11 +19,6 @@ TmCard::Application.configure do
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
   # config.action_dispatch.rack_cache = true
 
-  #bug fix: Refused to display 'http://wedxt.com/s-1d0c' in a frame because it set 'X-Frame-Options' to 'SAMEORIGIN'.
-  #the reason is different domain from www.wedxt.com and wedxt.com
-  config.action_dispatch.default_headers[:'X-Frame-Options'] = "ALLOW-FROM http://wedxt.com"
-  config.action_dispatch.default_headers[:'X-Frame-Options'] = "ALLOW-FROM http://www.wedxt.com"
-
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = false
 
