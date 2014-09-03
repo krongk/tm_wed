544e5665527a988b2e3e9dcaaf1f105aa57f465d
krongk <kenrome@gmail.com>
2014-09-03 22:25:39 +0800

    updage login

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -5,7 +5,7 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def all
     user = User.from_omniauth(request.env["omniauth.auth"])
     if user.persisted?
-      sign_in_and_redirect user, notice: 'success'
+      sign_in_and_redirect user, notice: '登录成功'
     else
       session["devise.user_attributes"] = user.attributes
       redirect_to new_user_registration_url
